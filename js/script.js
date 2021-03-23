c738aff9f3fd39355fef658f47a0fa3214d7d92b
JacobGlitcher <79320963+JacobGlitcher@users.noreply.github.com>
2021-03-23 16:43:53 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function headerBurger() {
+    document.getElementById('burger__menu').classList.toggle('burger-toggle-on')
+    document.getElementById('header__nav').classList.toggle('nav-toggle-on')
+}
+
+function headerMedia() {
+    document.getElementById('header__sign-media').classList.toggle('sign-media-toggle-on')
+    document.getElementById('header__sign').classList.toggle('sign-toggle-on')
+}
